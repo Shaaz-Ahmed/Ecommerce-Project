31f2c4cdede8ed4c0ca4eced2b2082cfd4a75874
Shaaz-Ahmed <shaazmukadam1616@gmail.com>
2024-10-18 15:53:22 +0530

    setup for shopify project

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../assets/images/logo.png';
+
+const MyHeader = ()=>{
+    return(
+        <>
+            <div className='container'>
+                 
+            </div>
+        </>
+    )
+};
+
+export default MyHeader;
