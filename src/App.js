cb3437f0b463e1dcee6d631709353980cccec7a4
Shaaz-Ahmed <shaazmukadam1616@gmail.com>
2024-10-21 14:34:13 +0530

    added banner of pharmacy

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from "./Components/Header";
 import Footer from './Components/Footer';
 import Banner from './Components/Banner';
 import Topbanner from './Components/Topbanner';
-// import Navbar from './Components/Navbar';
 
 const App = () =>{
   return(
