7a02b01128d5f0eb59297bf59919d155eb73836d
Shaaz-Ahmed <shaazmukadam1616@gmail.com>
2024-10-22 14:02:31 +0530

    added props for categories card

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import Paan from '../assets/images/Paan-corner.avif';
 
-const Categories = () =>{
-    return(
+const Categories = (props) => {
+    return (
         <>
             <div className='categories'>
                 <div className='cat-img'>
                     <div className='row'>
                         <div className='col-1'>
-                        <img src={Paan} alt='paan-photo'/>
+                            <img 
+                                src={props.imgsrc} 
+                                alt='paan-photo' 
+                                style={{ width: '150px', height: '170px' }} 
+                            />
                         </div>
                     </div>
                 </div>
@@ -18,3 +21,4 @@ const Categories = () =>{
 };
 
 export default Categories;
+
---
+++ src/Components/CategoriesData.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Paan  from '../../src/assets/images/Categories/Paan.avif';
+
+const CategoriesData = [
+    {
+        imgsrc:Paan
+    },
+
+    {
+        imgsrc:Paan
+    },
+    
+    {
+        imgsrc:Paan
+    },
+    
+    {
+        imgsrc:Paan
+    },
+
+    {
+        imgsrc:Paan
+    },
+
+    {
+        imgsrc:Paan
+    },
+
+    {
+        imgsrc:Paan
+    },
+
+    {
+        imgsrc:Paan
+    },
+
+    {
+        imgsrc:Paan
+    },
+
+    {
+        imgsrc:Paan
+    },
+    
+];
+
+export default CategoriesData;
